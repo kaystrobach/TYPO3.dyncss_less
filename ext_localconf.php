b1bfa23f75df8b92e2e96887c56bee5f646632ec
Kay Strobach <info@kay-strobach.de>
2016-04-09 18:31:19 +0200

    [TASK] fix loading problem of registry class

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,3 +1,5 @@
 <?php
 
-\KayStrobach\Dyncss\Configuration\BeRegistry::get()->registerFileHandler('less', 'KayStrobach\DyncssLess\Parser\LessParser');
+if(class_exists('\KayStrobach\Dyncss\Configuration\BeRegistry')) {
+    \KayStrobach\Dyncss\Configuration\BeRegistry::get()->registerFileHandler('less', 'KayStrobach\DyncssLess\Parser\LessParser');
+}
